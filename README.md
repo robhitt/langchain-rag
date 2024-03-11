d40048a955106bde2f0c00c31948a57dc7b7c3e3
rob.h <rob.h@taboola.com>
2024-03-10 23:01:26 -0400

    initial chat creation

---
+++ README.md
@@ -0,0 +1,2 @@
+* Tutorial: https://www.youtube.com/watch?v=LhnCsygAvzY
+* Code: https://github.com/pinecone-io/examples/blob/master/learn/generation/langchain/rag-chatbot.ipynb
--- main.py
+++ main.py
@@ -1,4 +1,28 @@
 import os
+from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
+from langchain.schema import (
+    SystemMessage,
+    HumanMessage,
+    AIMessage
+)
 
-os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+load_dotenv()
+
+os.environ["OPENAI_API_KEY"] = os.environ.get("OPENAI_API_KEY")
+
+chat = ChatOpenAI(
+    openai_api_key=os.environ["OPENAI_API_KEY"],
+    model='gpt-3.5-turbo'
+)
+
+messages = [
+    SystemMessage(content="You are a helpful assistant."),
+    HumanMessage(content="Hi AI, how are you today?"),
+    AIMessage(content="I'm great thank you. How can I help you?"),
+    HumanMessage(content="I'd like to understand string theory.")
+]
+
+res = chat(messages)
+print(res)
+
